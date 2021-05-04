05d941aefc26da7ecd046e733be84ee0418d664e
jesusgm1 <72188507+jesusgm1@users.noreply.github.com>
2021-05-04 22:13:11 +0200

    Add files via upload

---
+++ CalculadoraJesus/README.md
@@ -0,0 +1,24 @@
+# Calculadora
+Calculadora creada por Fabian Matilla en el entorno de Android Studio.
+
+# Preview:
+<h3> Diseño </h3>
+<img src="images/Diseño.PNG" />
+<h3> Vista Previa </h3>
+<img src="images/Calculadora_A_0.PNG" />
+<h3> Suma </v>
+<img src="images/Suma.PNG" />
+<h3> Resta </h3>
+<img src="images/Resta.PNG" />
+<h3> Multiplicacion </h3>
+<img src="images/Multiplicacion.PNG" />
+<h3> Division </h3>
+<img src="images/Division.PNG" />
+
+
+# Utilizacion :
+
+```
+Su utilizacion Basica seria introducir dos numeros y el resultado se veria en el textView superior
+
+```
---
+++ CalculadoraJesus/app/src/main/java/com/neptunosl/calculadora/MainActivity.java
@@ -0,0 +1,155 @@
+package com.neptunosl.calculadora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.CharArrayWriter;
+import java.io.PrintWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    PrintWriter operando;
+
+    private Button btn_suma;
+    private Button btn_resta;
+    private Button btn_division;
+    private Button btn_multiplicacion;
+    private Button botonPunto;
+    private Button boton0;
+    private Button boton1;
+    private Button boton2;
+    private Button boton3;
+    private Button boton4;
+    private Button boton5;
+    private Button boton6;
+    private Button boton7;
+    private Button boton8;
+    private Button boton9;
+
+    private TextView text_respuesta;
+
+    private EditText edit_numero_uno;
+    private EditText edit_numero_dos;
+
+    public void boton0(){
+        operando.append("0");
+    }
+    public void boton1(){
+        operando.append("1");
+    }
+    public void boton2(){
+        operando.append("2");
+    }
+    public void boton3(){
+        operando.append("3");
+    }
+    public void boton4(){
+        operando.append("4");
+    }
+    public void boton5(){
+        operando.append("5");
+    }
+    public void boton6(){
+        operando.append("6");
+    }
+    public void boton7(){
+        operando.append("7");
+    }
+    public void boton8(){
+        operando.append("8");
+    }
+    public void boton9(){
+        operando.append("9");
+    }
+    public void botonPunto(){
+        operando.append(".");
+    }
+    public void botonEsc(){
+        finish();
+    }
+    public void finish(){
+        Intent salida=new Intent( Intent.ACTION_MAIN);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text_respuesta = findViewById(R.id.respuesta);
+
+        edit_numero_uno = findViewById(R.id.numero1);
+        edit_numero_dos = findViewById(R.id.numero2);
+
+
+
+
+
+
+        btn_suma = findViewById(R.id.button_suma);
+        btn_suma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text_respuesta.setText( suma( Integer.parseInt(edit_numero_uno.getText().toString()),Integer.parseInt(edit_numero_dos.getText().toString()) )+"");
+            }
+        });
+
+        btn_division = findViewById(R.id.button9);
+        btn_division.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text_respuesta.setText( division( Integer.parseInt(edit_numero_uno.getText().toString()),Integer.parseInt(edit_numero_dos.getText().toString()) )+"");
+
+            }
+        });
+
+        btn_multiplicacion = findViewById(R.id.button_multiplicacion);
+        btn_multiplicacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text_respuesta.setText( multiplicacion( Integer.parseInt(edit_numero_uno.getText().toString()),Integer.parseInt(edit_numero_dos.getText().toString()) )+"");
+
+            }
+        });
+
+        btn_resta = findViewById(R.id.button_resta);
+        btn_resta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text_respuesta.setText( resta( Integer.parseInt(edit_numero_uno.getText().toString()),Integer.parseInt(edit_numero_dos.getText().toString()) )+"");
+
+            }
+        });
+
+    }
+
+    public double suma (int a, int b){
+        return a+b;
+    }
+
+    public double resta (int a, int b){
+        return a-b;
+    }
+
+    public double multiplicacion (int a, int b){
+        return a*b;
+    }
+
+    public double division (int a, int b){
+        int respuesta = 0;
+
+        if (b!=0){
+            respuesta=a/b;
+        }
+
+        return respuesta;
+    }
+
+
+}
